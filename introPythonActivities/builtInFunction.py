7c375d7472dda1d4d9c15d64bb7cfb07da61077b
ALFaulk <144712461+ALFaulk@users.noreply.github.com>
2023-09-20 16:34:00 +0000

    practice

--- introPythonActivities/builtInFunction.py
+++ introPythonActivities/builtInFunction.py
@@ -1,3 +1,12 @@
-name = 'Aariz Faulk'
+#number= False
+#print(type(number))
 
-type(name)
+#numberOfBooks = 3490909
+#print(float(numberOfBooks))
+
+#numberOfPencils = 1242.023343
+#print(int(numberOfPencils))
+
+fraction_of_pizza ='.9999999999'
+print(type(container))
+
