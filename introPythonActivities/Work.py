bcbf06c8df0ff65952d2e3c672e780e5bcaa7e23
ALFaulk <144712461+ALFaulk@users.noreply.github.com>
2023-10-30 19:21:21 +0000

    10/30/23 work

---
+++ introPythonActivities/Work.py
@@ -0,0 +1,39 @@
+#MONDAY ASYNC ACTIVITY
+
+#Please visit and read the articles found on the links below:
+
+#- Documentation
+#https://www.writethedocs.org/guide/writing/beginners-guide-to-docs/#:~:text=Your%20first%20steps%20in%20documentation,will%20serve%20your%20project%20well.
+
+#- Git Commit Messages
+#https://dev.to/chrissiemhrk/git-commit-message-5e21
+
+#When you are finished reading the articles, answer the following questions and commit and push 
+#your work to your repository. 
+
+#Your document should be named as followed async_Work_oct30.txt.
+
+#This assignment is due by the end of class in order to get credit. 
+#_______________________________________________________________________________
+
+#1. List and explain three 3 reasons why programmers write documentation. 
+
+'They are going to use the code in 6 months.'
+'They want people to understand it.'
+'They can go back to them code.'
+
+#2. In your own words, describe what a README.md document is.
+
+'A README.md Document is a file that describes what you put in your directory.'
+
+#3. Based on what your read, what are four (4) things that should be in a README document. 
+
+'The 4 thing that are needed in a README document are a brief, but solid description, the part of the project you are on, how to install, and the requirements needed.'
+
+#4. Name and describe three (3) reasons why programmers write commmit messages when using GitHub.
+
+'The 3 reasons are to describe what changes are made, where the changes are located, and what happened so the viewer can know what to look at.'
+
+#5. Describe three things (3) negative outcomes that could happen if a programmer has bad or no documentation.
+
+'The viewer sometimes might not know where to look, they might be mislead when it comes to whats going on, and might not know how to install the program if they are fond of it.'
