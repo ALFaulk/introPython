d47915e93584036871c4a57a82b59ee7bf822fd7
ALFaulk <144712461+ALFaulk@users.noreply.github.com>
2023-11-14 00:03:54 +0000

    done forgot to submit

---
+++ introPythonActivities/Async11/13/23.py
@@ -0,0 +1,14 @@
+#breifly describe what your program is about?
+'My program is called codeebot and it is designed to transfer coding information automatically as soon as the info is uploaded.' 
+
+##What probblem is your program trying to solve?
+'There are several problems my program aims to solve slow coding transfers and help build and translate undiscovered code.'
+
+#How will your program solve this problem?
+'Codeebot will solve the problems listed above in few ways the most prominant ways is by knowing how to disipher all code and copy it into differentv coding languages.'
+
+#Are there any apps/programs that similar to your program? (be descriptive)
+'There are several programs that inspired my program they are the translate functions in google mixed with the knowledge of coders all around the world and the copy and paste function.'
+
+#Who is this program designed for? Please describe the user who will be useing the program by describing three(3) things about them and how it relates to them using the program.
+'The general user for this program are students who are trying to disipher different codes andlearn new coding languages, coders who are finding hidden older codding ways, and adults who want to learn about the large world of code.'
