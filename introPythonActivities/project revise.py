ee4bab73404d23b64816d2250196f790b45e7d40
ALFaulk <144712461+ALFaulk@users.noreply.github.com>
2023-11-17 17:19:30 +0000

    done

---
+++ introPythonActivities/project revise.py
@@ -0,0 +1,14 @@
+#breifly describe what your program is about?
+'My program is call Smart Kitchen about your refrigerator gettting you food based on you mood just by it checking your tone of voice, the Smark Kitchen will activate when the words (Smart Kitchen...)  are spoke and it will sense you mood through you voice and get you your food.'
+
+#What probblem is your program trying to solve?
+'My program aims to solve not knowing what you want to eat becsuse of your mood.'
+
+#How will your program solve this problem?
+'Smart Kitchen will solve this problem by sensing the tone of voice.'
+
+#Are there any apps/programs that similar to your program? (be descriptive)
+'My program is inspired by alexa and siri because I got the voice activation feature from them.'
+
+#Who is this program designed for? Please describe the user who will be useing the program by describing three(3) things about them and how it relates to them using the program.
+'The general user for this program are chefs who are trying to creast new recipes based off of there mood, hospital workers who dont know what they want to eat aftter a long shift at work, and everyday people who are kinda lazy and dont wanna check their fridge for food.'
