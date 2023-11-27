3b5e453c4062cf5fc6806bfdb912a0bb69869d30
ALFaulk <144712461+ALFaulk@users.noreply.github.com>
2023-11-27 17:09:06 +0000

    done

---
+++ introPythonActivities/ProjectSmartFridge.py
@@ -0,0 +1,21 @@
+user_item= input('what is your mood: ')
+
+while user_item != 'mad':
+    print('that is not a mood')
+    user_item= input('what is your mood:')
+
+print('heres a cookie') 
+
+user_item= input('what is your mood: ')
+
+while user_item != 'sad':
+    print('that is not a mood')
+    user_item= input('what is your mood:')
+
+print('heres some ice cream') 
+
+while user_item != 'happy':
+    print('that is not a mood')
+    user_item= input('what is your mood:')
+
+print('heres some pizza')
